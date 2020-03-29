7032bf85ca7205169e7a55e6b294b57ae0bf40ab
coltonteefy <coltonteefy@yahoo.com>
2020-03-29 14:20:45 -0700

    adding previous list

--- src/App.js
+++ src/App.js
@@ -1,77 +1,75 @@
 import React, {Component} from 'react';
 import './App.css';
 import ChangeCategory from "./components/Change-Category";
+import PreviousGifs from "./components/Previous-Gifs";
 
-// PUBLIC_KEY = 'dc6zaTOxFJmzC';
-
-let API_KEY = 'Zbt4XDP6iVfN4sF30tTLrLXS5tSkE5MN';
-let category = 'trippy';
-let search = `http://api.giphy.com/v1/gifs/random?tag=${category}&api_key=${API_KEY}&limit=20`;
-
-function fetchData(search) {
-    fetch(search)
-        .then(res => {
-            return res.json()
-        })
-        .then(data => {
-            document.getElementById("image").src = data.data.image_url;
-        })
-        .catch(err => {
-            console.log(err)
-        })
-}
+const API_KEY = 'Zbt4XDP6iVfN4sF30tTLrLXS5tSkE5MN';
+let SEARCH_URL = `http://api.giphy.com/v1/gifs/random?tag=trippy&api_key=${API_KEY}&limit=20`;
 
 
 
 class App extends Component {
-
+    myVar;
 
     state = {
-        category: 'trippy'
+        category: 'trippy',
+        rating: 'PG-13',
+        previousGifs:[]
     };
 
-    search = `http://api.giphy.com/v1/gifs/random?tag=${this.state.category}&api_key=${API_KEY}&limit=20`;
-
-    myVar = setInterval(this.myTimer, 15000);
-
-    async myTimer() {
-        fetchData(search)
-    }
+    myTimer = () => {
+        this.fetchData(SEARCH_URL);
+    };
 
     componentDidMount() {
-        fetchData(search);
+        this.fetchData(SEARCH_URL);
         this.eventListener();
+        this.myVar = setInterval(this.myTimer, 5000);
     }
 
     changeCategory = (input) => {
-         this.setState({
+        this.setState({
             category: input
+        }, () => {
+            SEARCH_URL = `http://api.giphy.com/v1/gifs/random?tag=${this.state.category}&rating=${this.state.rating}&api_key=${API_KEY}&limit=20`;
+            this.fetchData(SEARCH_URL);
         });
     };
 
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        search = `http://api.giphy.com/v1/gifs/random?tag=${this.state.category}&api_key=${API_KEY}&limit=20`;
-        document.getElementById("image").src = "";
-        fetchData(search);
-    }
+    next = () => {
+        this.fetchData(SEARCH_URL);
+    };
 
-    next() {
-        document.getElementById("image").src = "";
-        fetchData(search);
-    }
-
-    eventListener() {
+    eventListener = () => {
         window.addEventListener('keydown', (e) => {
             if(e.code === "ArrowRight" || e.keyCode === 39) {
                 this.next();
             }
         });
-    }
+    };
+
+    fetchData = (search) => {
+        fetch(search)
+            .then(res => {
+                return res.json()
+            })
+            .then(data => {
+                document.getElementById("image").src = data.data.image_url;
+
+                this.setState({
+                    previousGifs: [...this.state.previousGifs, data.data.image_url]
+                })
+            })
+            .catch(err => {
+                console.log(err)
+            });
+    };
 
     render() {
         return (
             <div className="App">
                 <div className="controls">
+                    <PreviousGifs previous={this.state.previousGifs[this.state.previousGifs.length-1]}/>
                     <ChangeCategory category={this.state.category} changeCategory={this.changeCategory}/>
                     <button className="next-btn" onClick={this.next}>NEXT GIF </button>
                 </div>
---
+++ src/components/Previous-Gifs.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react';
+import '../styles/Previous-Gif.css';
+
+class PreviousGifs extends Component {
+
+    state = {
+        prev: []
+    };
+
+    componentWillReceiveProps(nextProps, nextContext) {
+        if (this.props.previous !== undefined) {
+            this.setState({
+                prev: [this.props.previous, ...this.state.prev]
+            })
+        }
+    }
+
+    render() {
+        return (
+            <div className="previous-container">
+                <button className="view-previous-btn">PREVIOUS LIST</button>
+                <div className="previous-list-container">
+                    <ul className="previous-ul">
+                        {
+                            this.state.prev.map((link, index) => {
+                                return (
+                                    <li key={index + link}>
+                                        <img src={link} alt="loading"/>
+                                        <a href={link}>{link}</a>
+                                        <i className="fas fa-heart heart-icon"/>
+                                    </li>
+                                )
+                            })
+                        }
+                    </ul>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default PreviousGifs;
+
+/*
+ {
+                            this.state.prev.map(link => {
+                                return (
+                                    <li>{{link}}</li>
+                                )
+                            })
+                        }
+
+* */
