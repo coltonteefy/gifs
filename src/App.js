d5ac049610f733d2a6fff88f9cd9d9d57a1c45e3
coltonteefy <coltonteefy@yahoo.com>
2020-04-01 13:46:06 -0700

    add saved images

--- src/App.js
+++ src/App.js
@@ -6,16 +6,21 @@ import Rating from "./components/Rating";
 import ToggleAPILocal from "./components/Toggle-API-Local";
 
 const API_KEY = 'Zbt4XDP6iVfN4sF30tTLrLXS5tSkE5MN';
-let SEARCH_URL = `http://api.giphy.com/v1/gifs/random?tag=trippy&api_key=${API_KEY}`;
+let SEARCH_URL = `http://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
 
-let timer;
+let timer = null;
+
+//images.preview_gif.url
+//image_original_url
+//id
+//title
 
 class App extends Component {
     state = {
+        currentGif: "",
         category: 'trippy',
         rating: 'PG-13',
         previousGifs: [],
-        loading: false,
         source: "api"
     };
 
@@ -25,12 +30,17 @@ class App extends Component {
                 return res.json()
             })
             .then(data => {
-                console.log(data.data)
-                document.getElementById("image").src = data.data.image_original_url;
+                // console.log(data.data);
+                const obj = {
+                    id: data.data.id,
+                    title: data.data.title,
+                    url: data.data.image_original_url,
+                    preview: data.data.images.preview_gif.url
+                };
 
                 this.setState({
-                    previousGifs: [...this.state.previousGifs, data.data.image_original_url],
-                    loading: false
+                    previousGifs: [...this.state.previousGifs, obj],
+                    currentGif: data.data.image_original_url
                 })
             })
             .catch(err => {
@@ -38,22 +48,27 @@ class App extends Component {
             });
     };
 
-    startTimer() {
+    startTimer = () => {
         timer = setInterval(() => {
             this.fetchData(SEARCH_URL);
-        }, 5000);
-    }
+        }, 3000);
+    };
+
+    restartTimer = () => {
+        clearInterval(timer);
+        this.startTimer();
+    };
 
     componentDidMount() {
-        this.startTimer();
         this.fetchData(SEARCH_URL);
         this.eventListener();
+        this.startTimer();
+        console.log("LOCAL APP", JSON.parse(localStorage.getItem("savedGifs")))
     }
 
     changeCategory = (input) => {
         this.setState({
             category: input,
-            loading: true
         }, () => {
             SEARCH_URL = `http://api.giphy.com/v1/gifs/random?tag=${this.state.category}&rating=${this.state.rating}&api_key=${API_KEY}`;
             this.fetchData(SEARCH_URL);
@@ -61,12 +76,8 @@ class App extends Component {
     };
 
     next = () => {
-        this.setState({
-            loading: true
-        }, () => {
-            document.getElementById("image").src = "";
-            this.fetchData(SEARCH_URL);
-        });
+        this.fetchData(SEARCH_URL);
+        clearInterval(timer);
     };
 
     updateRating = (input) => {
@@ -97,14 +108,6 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                {
-                    this.state.loading &&
-                    <div className="loading-screen">
-                        <img src="https://media.giphy.com/media/LpKReA1uL35HmeZ8ge/giphy.gif" alt="" className="spinning-character"/>
-                        <img src="https://media.giphy.com/media/STfIgB6cQYmolkJAvG/giphy.gif" alt="" className="load-bar"/>
-                        <h2>LOADING</h2>
-                    </div>
-                }
                 <div className="controls">
                     <div className="left-side-selections">
                         <PreviousGifs previous={this.state.previousGifs[this.state.previousGifs.length - 1]}/>
@@ -119,17 +122,10 @@ class App extends Component {
                         <img src="https://media.giphy.com/media/Lo5vjpAi4G7JZNXI8D/giphy.gif" alt="" id="next-gif"/>
                     </div>
                 </div>
-                <img alt="loading" id="image"/>
+                <img src={this.state.currentGif} alt="loading" id="image"/>
             </div>
         );
     }
 }
 
 export default App;
-
-/*
-https://media.giphy.com/media/feN0YJbVs0fwA/giphy.gif
-https://media.giphy.com/media/11z8xTBzFylZ60/giphy.gif
-https://media.giphy.com/media/r09BeWEk9JZL2/giphy.gif
-https://media.giphy.com/media/STfIgB6cQYmolkJAvG/giphy.gif
-* */
--- src/components/Previous-Gifs.js
+++ src/components/Previous-Gifs.js
@@ -4,7 +4,9 @@ import '../styles/Previous-Gif.css';
 class PreviousGifs extends Component {
 
     state = {
-        prev: []
+        prev: [],
+        savedGifs: JSON.parse(localStorage.getItem("savedGifs")),
+        currentSection: "archived"
     };
 
     componentWillReceiveProps(nextProps, nextContext) {
@@ -15,33 +17,121 @@ class PreviousGifs extends Component {
         }
     }
 
-    saveGif(e) {
-        console.log(e.currentTarget)
-    }
+    saveGif = (e) => {
+        let saveData = {};
+        let prev = [];
+        this.state.prev.filter(data => {
+            if(data.id === e.target.id) {
+                saveData = data;
+            } else {
+                prev.push(data);
+            }
+        });
 
-    render() {
-        return (
-            <div className="previous-container">
-                <button className="view-previous-btn">
-                    <i className="fas fa-archive"></i> archived
-                </button>
-                <div className="previous-list-container">
-                    <img src="https://media.giphy.com/media/W2PcRKgR80TlC6El8D/giphy.gif" alt="throw back" id="throwback"/>
+        let id = e.target.id;
+        document.getElementById(id).classList.add("saved");
+
+        this.setState({
+            prev: [...prev],
+            savedGifs: [...this.state.savedGifs, saveData],
+        }, () => {
+            localStorage.setItem("savedGifs", JSON.stringify(this.state.savedGifs))
+        })
+
+
+    };
+
+    heartColor = (id) => {
+        if (this.state.savedGifs.includes(id)) {
+            return <i className="fas fa-heart heart-icon saved" id={id}/>
+        }
+
+        return <i className="fas fa-heart heart-icon" id={id}/>
+    };
+
+    toggleSavedArchive = (e) => {
+        let archived = document.getElementById("archived-btn");
+        let saved = document.getElementById("saved-btn");
+
+        let archSection = document.getElementById("archived-section");
+        let savedSection = document.getElementById("saved-section");
+
+        if (e.target.innerHTML === "ARCHIVED") {
+            archived.classList.add("previous-selected");
+            saved.classList.remove("previous-selected");
+            this.setState({
+                currentSection: "archived"
+            })
+        } else {
+            archived.classList.remove("previous-selected");
+            saved.classList.add("previous-selected");
+            this.setState({
+                currentSection: "saved"
+            })
+        }
+    };
+
+    toggleSections() {
+        if (this.state.currentSection === "archived") {
+            return (
+                <section id="archived-section">
                     <ul className="previous-ul">
                         {
-                            this.state.prev.map((link, index) => {
+                            this.state.prev.map((objData, index) => {
                                 return (
                                     index < 15 &&
-                                    <li key={index + link} onClick={this.saveGif}>
-                                        <img src={link} alt="loading"/>
-                                        <a href={link}>{link}</a>
-                                        <i className="fas fa-heart heart-icon"/>
+                                    <li key={index + objData.id} onClick={this.saveGif}>
+                                        <img src={objData.preview} alt="loading"/>
+
+                                        {
+                                            this.heartColor(objData.id)
+                                        }
                                     </li>
 
                                 )
                             })
                         }
                     </ul>
+                </section>
+            )
+        } else {
+            return (
+                <section id="saved-section">
+                    <ul className="saved-ul">
+                        {
+                            this.state.savedGifs.map((objData, index) => {
+                                return(
+                                    <li key={index + objData.id}>
+                                        <img src={objData.preview} alt="loading"/>
+                                    </li>
+                                )
+                            })
+                        }
+                    </ul>
+
+                </section>
+            )
+        }
+    }
+
+    render() {
+        return (
+            <div className="previous-container">
+                <button className="view-previous-btn">
+                    <i className="fas fa-archive"> </i> archived
+                </button>
+                <div className="previous-list-container">
+                    <img src="https://media.giphy.com/media/W2PcRKgR80TlC6El8D/giphy.gif" alt="throw back"
+                         id="throwback"/>
+                    <div className="save-archive-toggle" onClick={this.toggleSavedArchive}>
+                        <h3 id="archived-btn" className="previous-selected">ARCHIVED</h3>
+                        <h3 id="saved-btn">SAVED</h3>
+                    </div>
+                    <div className="archived-body-section">
+                        {
+                            this.toggleSections()
+                        }
+                    </div>
                 </div>
             </div>
         );
